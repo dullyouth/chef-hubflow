4122105c7433dbcb7b37dffa1d250d5875adb5d0
Chris <chris@mattr.co>
2015-03-12 21:09:15 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'hubflow'
 maintainer       'Chris Molle'
-maintainer_email 'chris@nocplace.com'
+maintainer_email 'chris@mattr.co'
 license          'Apache 2.0'
 description      'Installs/Configures hubflow'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
